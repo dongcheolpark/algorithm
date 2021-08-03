5e9fc9fd6376ebbb86db760f511e125f1aaa62a3
dongcheolpark <head02325@soongsil.ac.kr>
2021-08-03 23:19:00 +0900

    django로 c++ 프로세스 실행 성공

--- src/back-end/back_end/sort/views.py
+++ src/back-end/back_end/sort/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import subprocess
 
 @csrf_exempt
 def sort(request):
+    subprocess.run(['./program','10','1'])
     result = open('out.json')
     return HttpResponse(result)
